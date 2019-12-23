d9c65bc713f8df149f5cd1e70e7ddb40f55eb9c7
Steve Hunt <steve@brandwatch.com>
2019-12-23 15:54:16 +0000

    Collecting everything into string vectors

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,7 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-// use std::collections::HashMap;
 
-// struct BigramIndex {
-//     bigram: HashMap<String, Option<Box<BigramIndex>>>,
-// }
-
-struct WordStore {
-    word_store : Vec<Vec<String>>,
-}
-
-impl WordStore {
-    pub fn new() -> WordStore {
-        let size = 10;
-        // vec! macro would be useful here!
-        let mut word_store : Vec<Vec<String>> = Vec::with_capacity(size);
-        for _ in 0..size {
-            let new_store : Vec<String> = Vec::new();
-            word_store.push(new_store);
-        }
-        WordStore {
-            word_store,
-        }
-    }
-
-    pub fn add(&mut self, word : String) {
-        let word_len : usize = word.len();
-
-        // exit if the word has an uneven size
-        if word_len % 2 == 1 { return };
-
-        // extend the vector if needs be
-        let index_pt = (word_len / 2) - 1;
-
-        if index_pt >= self.word_store.len() {
-            // panic!("Word store len is less than {}", index_pt);
-            println!("Growing the wordstore");
-            self.word_store.resize_with(index_pt+1, || {Vec::new()});
-        }
-
-        // insert the word into the word_store
-        self.word_store[index_pt].push(word);
-    }
-
-    pub fn print(&self) {
-        for (index, array) in self.word_store.iter().enumerate() {
-            println!("{}", index);
-            for word in array {
-                println!("  - {}", word);
-            }
-        }
-    }
-}
+use puzzler::WordStore;
 
 
 fn main() {
